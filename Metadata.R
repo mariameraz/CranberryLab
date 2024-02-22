ff2ea24e55ac8bb548df1f76139e27912256b306
Alejandra Torres <ma.torresmeraz@gmail.com>
2024-02-22 15:12:05 -0600

    Update Metadata.R

--- Metadata.R
+++ Metadata.R
@@ -1,8 +1,5 @@
 setwd('~/Documents/Harvest_2023/Diversity_Panel_H2023/DP_H2/')
 
-files <- list.files(pattern = '.jpg')
-file.rename(files, gsub('\\(|\\)|\\_|\\-','',files))
-
 setwd('/home/zalapa/Downloads/')
 
 data <- read.csv('VAC_155005_Normal_Plate_Layout_02-08-2023_14-44-04.csv', header = T)
