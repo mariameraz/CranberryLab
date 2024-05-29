3f4cc5aa0008881fcfec43efb67c236179b902c9
Alejandra Torres <ma.torresmeraz@gmail.com>
2024-05-29 16:41:32 -0500

    Update vcf_QC.R

--- vcf_QC.R
+++ vcf_QC.R
@@ -1,5 +1,7 @@
 library(vcfR)
+library(tidyverse)
 
+setwd('cran/VCFs/')
 # LOADING SNPs DATA
 vcf <- read.vcfR('filtered.vcf', convertNA = T)
 ## Meta lines: 73
@@ -9,11 +11,19 @@ vcf <- read.vcfR('filtered.vcf', convertNA = T)
 
 # EXTRACT GENOTYPES
 snps_num <- extract.gt(vcf, 
-           element = "GT",
-           IDtoRowNames  = F,
-           as.numeric = T,
-           convertNA = T,
-           return.alleles = F)
+                       element = "GT",
+                       IDtoRowNames  = F,
+                       as.numeric = T,
+                       convertNA = T,
+                       return.alleles = F)
+gt_info <- snps_num
 
+# How many NAs are for each sample?
+na_info <- colSums(is.na(gt_info)) 
+summary(na_info)
+
+# Remove samples with more than 25% NAs
+# 25% threslhold: 
+round(nrow(gt_info) * 0.25)
 
 gt_info %>% str_subset(., '/') %>% table
