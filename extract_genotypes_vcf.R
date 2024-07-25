3a3e81842d86cf856efff8b52cd905bc93b1c264
Alejandra Torres <ma.torresmeraz@gmail.com>
2024-07-25 18:00:50 -0500

    Update and rename vcf_qc.R to extract_genotypes_vcf.R

--- extract_genotypes_vcf.R
+++ extract_genotypes_vcf.R
@@ -1,12 +1,8 @@
 # Load libraries
 library(vcfR)
-library(vegan)
 library(ggplot2)
-library(ggpubr)
-library(factoextra)
 library(tidyverse)
-library(ggrepel)
-library(RColorBrewer)
+
 
 # Set working directory
 setwd('~/VACCap/')
@@ -21,7 +17,7 @@ vcf <- read.vcfR('filtered.vcf', convertNA = TRUE)
 gt.data <- extract.gt(vcf, 
                        element = "GT", # extract genotypes
                        IDtoRowNames  = F, # Do not use the ID column from the FIX region as rownames
-                       as.numeric = F, # Should the matrix be converted to numeric? >>>> Be careful in this step, may be you should look at the no transformated data first. Find more info in vcfR documentation.
+                       as.numeric = F, # Should the matrix be converted to numeric?
                        convertNA = T, # '.' are converted to 'NA'
                        return.alleles = F) # logical indicating whether to return genotypes (0/1) or alleles (A/T)
 
@@ -35,7 +31,7 @@ gt.data.backup <- gt.data
 samples <- read.csv('samples.csv', header = T)
 samples[duplicated(samples$Unique.Client.Code),]
 
-# Delete duplicated sample (!!! before to do this, I double checked that it is trully the same sample)
+# Delete duplicated sample (!!! before to do this, I double checked that it was trully the same sample)
 gt.data <- gt.data %>%
                 as.data.frame() %>%
                 select(-VAC_155005_P004_WB09)
@@ -50,11 +46,11 @@ new.colnames <- samples %>%
 rename.vector <- setNames(samples$Unique.Client.Code[samples$RAPiD.Genomics.Sample.Code %in% colnames(gt.data)],
                           samples$RAPiD.Genomics.Sample.Code[samples$RAPiD.Genomics.Sample.Code %in% colnames(gt.data)])
 
-gt.data.rename <- gt.data %>%
+gt.data <- gt.data %>%
                     as.data.frame() %>%
                     rename_with(~ rename.vector[.x], .cols = colnames(gt.data))
 
-head(gt.data.rename)
+head(gt.data)
 
 # How many NAs are in each sample (column)?
 na.data <- is.na(gt.data) %>%
@@ -63,8 +59,19 @@ na.data <- is.na(gt.data) %>%
   dplyr::rename(na_value = '.')
 
 
+# Create a new column with the % of NAs to facilitate the visualization
+na.data <- na.data %>% mutate(perc_na = round((na_value/280603)*100, digits = 1))
 
+# Create a boxplot to observe the distribution of the NA data
 ggplot(na.data, aes(x = na_value)) +
   geom_boxplot()
+
+thres <- na.data %>% arrange(desc(na_value)) %>% filter(perc_na < 25)
+
+# Deleting samples with more than 25% of NAs
+gt.data <- gt.data[,colnames(gt.data) %in% rownames(thres)] 
+
+# Save genetic data
+write.table(gt.data, 'gt.data.txt', quote = F)
 
 
