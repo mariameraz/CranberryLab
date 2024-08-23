d47c53ea532f255fc50cd55ff2deba6ea731b9b1
Alejandra Torres <ma.torresmeraz@gmail.com>
2024-08-22 23:34:25 -0500

    Create mainparams.R
    Create mainparams file for STRUCTURE

---
+++ Structure/mainparams.R
@@ -0,0 +1,10 @@
+mainparams(maxpops = 3, 
+           infile = 'dp_struc.txt',
+           outfile = 'mainparams',
+           label = 1, 
+           popdata = 0,
+           popflag = 0,
+           numloci = 27745,
+           numinds = 242,
+           outpath = '/Users/alejandra/Documents/CranLab/DiversityPanel_PopStruc/Structure/mainparams.txt'
+           )
