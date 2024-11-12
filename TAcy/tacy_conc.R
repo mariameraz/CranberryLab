1d227e885d7b8ee1ed4951f4fff2eeb99c239fbe
Alejandra Torres <ma.torresmeraz@gmail.com>
2024-11-12 15:31:34 -0600

    Update tacy_conc.R

--- TAcy/tacy_conc.R
+++ TAcy/tacy_conc.R
@@ -2,7 +2,7 @@ tacy_conc <- function(data){
   # Calculate A from data frame:
   data %>% 
     mutate(Diff = as.numeric(nm_520) - as.numeric(nm_700)) %>%
-    dplyr::select(pH, Rep, Sample_code, Diff) %>% 
+    dplyr::select(pH, Rep, Sample_code, Diff, Harvest) %>% 
     group_by(Sample_code, Rep, Harvest) %>%
     pivot_wider(names_from = pH, 
                 values_from = Diff, 
@@ -11,6 +11,5 @@ tacy_conc <- function(data){
       A = pH_1 - pH_4.5,
       Concentration = round((A * 449.2 * 5 * 10^3) / (26900 * 1), digits = 3)
     ) %>% 
-    ungroup() %>%
-    select(Sample_code, Rep, A, Concentration)
+    ungroup() 
 }
