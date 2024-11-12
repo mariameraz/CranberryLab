a8ba0e8f6c336eac0cb6d59dc06097dfb27849ed
Alejandra Torres <ma.torresmeraz@gmail.com>
2024-11-12 15:24:58 -0600

    Update Fungus_Biodiversity.R

--- Fungus_Biodiversity/Fungus_Biodiversity.R
+++ Fungus_Biodiversity/Fungus_Biodiversity.R
@@ -6,16 +6,16 @@ setwd('~/Documents/GitHub/CranberryLab/Fungus_Biodiversity/')
 
 # Load data
 fungus_list <- read.csv('Fungus_list.csv',header = T) %>% select(-Details)
-
-View(fungus_list)
 abundance <- read.csv('allsample.all.reabundance.csv', header = T)
 
 abundance$Species <- gsub('^.*_(.*)$', '\\1', abundance$Species)
 
-fungus_list[(fungus_list$Genus %in% abundance$Genus),]
+# Which species in the fungus list are present in the abundance table?
+fungus_list[(fungus_list$Species %in% abundance$Species),]
 
- 
-abundance[(abundance$Genus %in% fungus_list$Genus),] %>%
+# Which species in the abundance table are also present in the fungus list? 
+temp <- abundance[(abundance$Genus %in% fungus_list$Genus),] %>%
   select(Phylum, Class, Order, Family, Genus, Species)
 
+# Save abundance filtered data
 write.csv(temp, 'Genus_present.csv', row.names = F, quote = F)
