2b2d1cf53a50be780fac45611ea68bc88608eea2
Alejandra Torres <ma.torresmeraz@gmail.com>
2024-11-14 17:16:54 -0600

    Update fastqc.sh

--- Metabarcoding_HoneyBees/fastqc.sh
+++ Metabarcoding_HoneyBees/fastqc.sh
@@ -5,12 +5,14 @@
 
 cd $PBS_O_WORKDIR
 
-module load FastQC/0.11.2
+module load FastQC/0.11.2 MultiQC/1.0
 
 for i in *R1_001.fastq.gz
 do
 SAMPLE=$(echo $i | sed 's/'R1_001\\.fastq\\.gz'//g')
 fastqc ${SAMPLE}R1_001.fastq.gz ${SAMPLE}R1_002.fastq.gz -o ./Fastqc_reports
 done
+
+multiqc ./Fastqc_reports
 
 
