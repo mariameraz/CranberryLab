5d7432436c87a088117c81e6fcfe16fd6e2dd4a5
Alejandra Torres <ma.torresmeraz@gmail.com>
2024-11-15 16:27:09 -0600

    commit

--- TAcy/Tacy_Analysis.R
+++ TAcy/Tacy_Analysis.R
@@ -31,14 +31,7 @@ source('tacy_conc.R')
 conc_data <- tacy_conc(data) %>% 
   left_join( metadata, by = 'Sample_code') 
 
-# Get the mean concentration value
-
-
-
-
-
-
-
+# Load the weight data
 weight_data <- read.csv('Weight_H1_2023.csv', header = TRUE)
 weight_data$Sample_name <- toupper(weight_data$Sample_name)
 weight_data$Sample_name <- gsub('[\\. ()]', '', weight_data$Sample_name)
@@ -50,12 +43,58 @@ h1_2023 <- weight_data[(weight_data$Sample_name %in% metadata$Sample_name),] %>%
 
 tacy_2023 <- conc_data %>% filter(Harvest == 1 )
 
+# Merge weight and tacy data
 tacy <- left_join(tacy_2023, 
           h1_2023, by = 'Sample_name', 
           relationship = 'many-to-many') %>% 
   mutate(Sample_Rep = paste(Sample_name, Rep, sep = '_')) %>% 
-  filter(Population == 'DiversityPanel') %>% View
+  filter(Population == 'DiversityPanel') 
 
+colnames(tacy)
+
+tacy_means <- tacy %>% 
+                  group_by(Sample_code) %>% 
+                  summarise(Mean_concentration = mean(Concentration),
+                            .groups = "drop") %>% 
+                  left_join(select(tacy, 
+                                   Sample_code, 
+                                   Sample_name, 
+                                   Fruit_n, 
+                                   Total_weight, 
+                                   Avg_weight),
+                            by = c("Sample_code")) %>%
+  distinct(Sample_code, .keep_all = TRUE) %>% 
+  column_to_rownames('Sample_code') %>% 
+  na.omit() %>%
+  filter(Mean_concentration >= 0)
+
+
+corr <- cor(tacy_means$Mean_concentration, 
+            tacy_means$Avg_weight, 
+            use = "complete.obs", 
+            method = "spearman")
+
+library(ggplot2)
+
+ggplot(tacy_means, aes(x = Avg_weight, y = Mean_concentration)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "blue") +
+  labs(title = "Relationship Between Avg Weight and Anthocyanin Concentration",
+       x = "Average Weight (g)",
+       y = "Mean Concentration (mg/250 µL)") +
+  annotate("text", 
+           x = max(tacy_means$Avg_weight, na.rm = TRUE), # Ajusta posición en el eje X
+           y = max(tacy_means$Mean_concentration, na.rm = TRUE), # Ajusta posición en el eje Y
+           label = paste("Correlation: ", round(corr, 3)), # Texto de la anotación
+           hjust = 1, vjust = 1, size = 4) +
+  theme_classic()
+
+hist(tacy_means$Mean_concentration)
+
+lm_model <- lm(Mean_concentration ~ poly(Avg_weight, 2), data = tacy_means)
+summary(lm_model)
+
+# Get the mean concentration value
 
 # write.csv(weight_data, 'Weight_H1_2023.csv', quote = F, row.names = F)
 # 
