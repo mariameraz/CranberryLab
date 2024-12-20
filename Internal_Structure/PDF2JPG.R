2f8f1b0fad7d01f638483a7155233b6c0d778f30
Alejandra Torres <ma.torresmeraz@gmail.com>
2024-12-20 15:46:16 -0600

    commit

--- Internal_Structure/PDF2JPG.R
+++ Internal_Structure/PDF2JPG.R
@@ -1,25 +1,30 @@
 library(pdftools)
 library(magick)
 
-setwd('~/Documents/GitHub/CranberryLab/External_structure/PDF')
+setwd('~/Documents/GitHub/CranberryLab/Internal_Structure/PDF')
 # Ruta del archivo PDF original
-input_pdf <- "lalo11.PDF"
+input_pdf <- "STAMPS_00.PDF"
 
 # Extraer todas las páginas como texto o imágenes
 total_pages <- pdf_length(input_pdf)
 
+# If Stamp_photos does not directory exist, create it.
+if (!dir.exists("Stamp_photos")) {
+  dir.create("Stamp_photos")
+}
+
+# Save the pdfs as pdfs
 for (i in 1:total_pages) {
   output_pdf <- sprintf("Stamp_photos/pagina_%d.pdf", i)
   pdf_subset(input_pdf, pages = i, output = output_pdf)
 }
 
-
 # Convert pdf to jpg
 
 # Carpeta donde están los PDFs
-input_dir <- "~/Documents/GitHub/CranberryLab/External_structure/PDF/Stamp_photos/"  # Cambia a la carpeta con tus PDFs
-output_dir <- "~/Documents/GitHub/CranberryLab/External_structure/PDF/Stamp_photos/JPG"  # Cambia a la carpeta donde quieres guardar las imágenes
-dir.create(output_dir, recursive = TRUE)  # Crea la carpeta de salida si no existe
+input_dir <- "~/Documents/GitHub/CranberryLab/Internal_structure/PDF/Stamp_photos/"  # Cambia a la carpeta con tus PDFs
+output_dir <- "~/Documents/GitHub/CranberryLab/Internal_structure/PDF/Stamp_photos/JPG"  # Cambia a la carpeta donde quieres guardar las imágenes
+dir.create(output_dir, showWarnings = F)  # Crea la carpeta de salida si no existe
 
 # Listar todos los archivos PDF en la carpeta
 pdf_files <- list.files(input_dir, pattern = "\\.pdf$", full.names = TRUE)
